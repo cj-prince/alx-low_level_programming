af721e65fc53443e5ce01ded6ae222b4de171084
Davids Chijioke Prince <77537310+cj-prince@users.noreply.github.com>
2022-03-15 20:24:01 +0100

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,31 +5,31 @@
  */
 void times_table(void)
 {
-    int i, j, res;
+	int i, j, res;
 
-    for (i = 0; i < 10; i++)
-    {
-        for (j = 0; j < 10; j++)
-        {
-            if (j == 0)
-            {
-                _putchar(48);
-                continue;
-            }
-            res = i * j;
-            _putchar(',');
-            _putchar(' ');
-            if (res >= 10)
-            {
-                _putchar(res / 10 + 48);
-                _putchar(res % 10 + 48);
-            }
-            else
-            {
-                _putchar(' ');
-                _putchar(res + 48);
-            }
-        }
-        _putchar('\n');
-    }
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (j == 0)
+			{
+				_putchar(48);
+				continue;
+			}
+			res = i * j;
+			_putchar(',');
+			_putchar(' ');
+			if (res >= 10)
+			{
+				_putchar(res / 10 + 48);
+				_putchar(res % 10 + 48);
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(res + 48);
+			}
+		}
+	_putchar('\n');
+	}
 }

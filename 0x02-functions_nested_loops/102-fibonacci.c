9b3a932d0d11ba72c5fe1072c3feba9546821f10
cj-prince <davids.chijioke@gmail.com>
2022-03-15 20:47:14 +0100

    fibonacci_code

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: always 0
+ */
+int main(void)
+{
+    int i, n;
+    long f1, f2, next;
+
+    f1 = 1;
+    f2 = 2;
+    n = 50;
+
+    for (i = 0; i < n; i++)
+    {
+        if (i == 49)
+        {
+            printf("%ld", f1);
+        }
+        else
+        {
+            printf("%ld, ", f1);
+            next = f1 + f2;
+            f1 = f2;
+            f2 = next;
+        }
+    }
+    printf("\n");
+    return (0);
+}
---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the first 98 Fibonacci numbers, starting with
+ *        1 and 2, separated by a comma followed by a space.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int i = 0;
+    unsigned long int a = 0, b = 1, next = 0;
+
+    while (i < 98)
+    {
+        next = a + b;
+        a = b;
+        b = next;
+        printf("%lu", next);
+
+        if (i < 97)
+            printf(", ");
+        i++;
+    }
+    putchar('\n');
+    return (0);
+}

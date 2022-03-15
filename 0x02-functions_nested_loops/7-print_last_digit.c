75732f0d72c45dbee81987f8f694c3806c213070
Davids Chijioke Prince <77537310+cj-prince@users.noreply.github.com>
2022-03-15 20:18:36 +0100

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,14 +6,14 @@
  */
 int print_last_digit(int n)
 {
-    int last;
+	int last;
 
-    last = n % 10;
-    if (last < 0)
-    {
-        _putchar(-last + 48);
-        return (-last);
-    }
-    _putchar(last + '0');
-    return (last);
+	last = n % 10;
+	if (last < 0)
+	{
+		_putchar(-last + 48);
+		return (-last);
+	}
+	_putchar(last + '0');
+	return (last);
 }

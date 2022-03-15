6771a377152477899f24f6420896329bffd63d5d
cj-prince <davids.chijioke@gmail.com>
2022-03-15 19:49:20 +0100

    print_to_98

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*add-adds to numbers i and j
+*@a:number 1
+*@b:number 2
+*Return: returns i + j
+*/
+int add(int a, int b)
+{
+    int result;
+
+    result = a + b;
+    return (result);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * times_table -  print a time table
+ * Return: void
+ */
+void times_table(void)
+{
+    int i, j, res;
+
+    for (i = 0; i < 10; i++)
+    {
+        for (j = 0; j < 10; j++)
+        {
+            if (j == 0)
+            {
+                _putchar(48);
+                continue;
+            }
+            res = i * j;
+            _putchar(',');
+            _putchar(' ');
+            if (res >= 10)
+            {
+                _putchar(res / 10 + 48);
+                _putchar(res % 10 + 48);
+            }
+            else
+            {
+                _putchar(' ');
+                _putchar(res + 48);
+            }
+        }
+        _putchar('\n');
+    }
+}

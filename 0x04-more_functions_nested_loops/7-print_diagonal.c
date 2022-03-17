22af082e57613cb5c3d166b0e6a615fc2df3e911
Davids Chijioke Prince <77537310+cj-prince@users.noreply.github.com>
2022-03-17 13:11:42 +0100

    checking betty

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,20 +5,20 @@
  */
 void print_diagonal(int n)
 {
-int i = 0, s;
+	int i = 0, s;
 
-if (n > 0)
-{
-    while (i < n)
-    {
-        for (s = 0; s < i; s++)
-            _putchar(' ');
+	if (n > 0)
+	{
+		while (i < n)
+		{
+			for (s = 0; s < i; s++)
+				_putchar(' ');
 
-        _putchar('\\');
-        _putchar('\n');
-        i++;
-    }
-}
-else
-    _putchar('\n');
+			_putchar('\\');
+			_putchar('\n');
+			i++;
+		}
+	}
+	else
+		_putchar('\n');
 }

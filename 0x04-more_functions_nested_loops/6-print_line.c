9734d2bc63a2da654a67af85db66c3883cd3a503
Davids Chijioke Prince <77537310+cj-prince@users.noreply.github.com>
2022-03-17 13:09:32 +0100

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,12 +7,12 @@
  */
 void print_line(int n)
 {
-int i = 0;
+	int i = 0;
 
-if (n > 0)
-{
-    for (; i < n; i++)
-        _putchar('_');
-}
-_putchar('\n');
+	if (n > 0)
+	{
+		for (; i < n; i++)
+			_putchar('_');
+	}
+	_putchar('\n');
 }

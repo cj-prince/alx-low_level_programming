bba1ba485cac88f17911f6b0f5d18327ea8eb204
Davids Chijioke Prince <77537310+cj-prince@users.noreply.github.com>
2022-03-17 12:26:08 +0100

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops0x04-more_functions_nested_loops/0-isupper.c
@@ -8,16 +8,5 @@
 
 int _isupper(int c)
 {
-	char uppercase = 'A';
-	int isupper = 0;
-
-	for (; uppercase <= 'Z'; uppercase++)
-	{
-		if (c == uppercase)
-		{
-			isupper = 1;
-			break;
-		}
-	}
-	return (isupper);
+	return (c >= 65 && c <= 90);
 }

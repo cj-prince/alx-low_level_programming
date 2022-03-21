eb42a19dcb0f5c75ae4c6df2dadfc5dba58f5651
Davids Chijioke Prince <77537310+cj-prince@users.noreply.github.com>
2022-03-21 16:42:27 +0100

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,14 +7,14 @@
  */
 void rev_string(char *s)
 {
-    int i, len;
-    char tmp;
+	int i, len;
+	char tmp;
 
-    len = strlen(s);
-    for (i = 0; i < (len / 2); i++)
-    {
-        tmp = s[i];
-        s[i] = s[len - i - 1];
-        s[len - i - 1] = tmp;
-    }
+	len = strlen(s);
+	for (i = 0; i < (len / 2); i++)
+	{
+		tmp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = tmp;
+	}
 }

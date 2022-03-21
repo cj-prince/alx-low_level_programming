a67900f4b5e9af23d291f61d6b08f2edb8bb50ff
Davids Chijioke Prince <77537310+cj-prince@users.noreply.github.com>
2022-03-21 17:36:25 +0100

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
  * _atoi - a function that converts string to integer
  * @s: An input string
@@ -6,28 +8,28 @@
  */
 int _atoi(char *s)
 {
-    int sign = 1;
-    unsigned int total = 0;
-    char null_flag = 0;
+	int sign = 1;
+	unsigned int total = 0;
+	char null_flag = 0;
 
-    while (*s)
-    {
-        if (*s == '-')
-            sign *= -1;
+	while (*s)
+	{
+		if (*s == '-')
+			sign *= -1;
 
-        if (*s >= '0' && *s <= '9')
-        {
-            null_flag = 1;
-            total = total * 10 + *s - '0';
-        }
+		if (*s >= '0' && *s <= '9')
+		{
+			null_flag = 1;
+			total = total * 10 + *s - '0';
+		}
 
-        else if (null_flag)
-            break;
-        s++;
-    }
+		else if (null_flag)
+			break;
+		s++;
+	}
 
-    if (sign < 0)
-        total = (-total);
+	if (sign < 0)
+		total = (-total);
 
-    return (total);
+	return (total);
 }

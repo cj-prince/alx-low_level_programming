4093be01ec3fe9286492c01043e816668ac2dc40
Davids Chijioke Prince <77537310+cj-prince@users.noreply.github.com>
2022-03-21 16:48:11 +0100

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,13 +10,13 @@
  */
 void print_array(int *a, int n)
 {
-    int i = 0;
+	int i = 0;
 
-    for (; i < n; i++)
-    {
-        printf("%d", a[i]);
-        if (i < n - 1)
-            printf(", ");
-    }
-    putchar('\n');
+	for (; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+			printf(", ");
+	}
+	putchar('\n');
 }

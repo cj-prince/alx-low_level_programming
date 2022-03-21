850ea0bad2cd18cf88f5a53c3137fc6b068b47eb
Davids Chijioke Prince <77537310+cj-prince@users.noreply.github.com>
2022-03-21 16:47:26 +0100

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,19 +6,19 @@
  */
 void puts_half(char *str)
 {
-    int len = 0, i, n;
+	int len = 0, i, n;
 
-    while (str[len] != '\0')
-        len++;
+	while (str[len] != '\0')
+		len++;
 
-    if (len % 2 == 0)
-        n = len / 2;
+	if (len % 2 == 0)
+		n = len / 2;
 
-    else
-        n = (len + 1) / 2;
+	else
+		n = (len + 1) / 2;
 
-    for (i = n; i < len; i++)
-        _putchar(str[i]);
+	for (i = n; i < len; i++)
+		_putchar(str[i]);
 
-    _putchar('\n');
+	_putchar('\n');
 }

f61d3fcbaa89100ae897339da58b748fd705670e
Davids Chijioke Prince <77537310+cj-prince@users.noreply.github.com>
2022-03-24 16:11:02 +0100

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,19 +8,19 @@ int check_seperators(char c);
  */
 char *cap_string(char *s)
 {
-    int i = 0;
+	int i = 0;
 
-    while (s[i])
-    {
-        if (i == 0 && (s[i] >= 'a' && s[i] <= 'z'))
-            s[i] -= 32;
+	while (s[i])
+	{
+		if (i == 0 && (s[i] >= 'a' && s[i] <= 'z'))
+			s[i] -= 32;
 
-        if (check_seperators(s[i]) && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
-            s[i + 1] -= 32;
-        i++;
-    }
+		if (check_seperators(s[i]) && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
+			s[i + 1] -= 32;
+		i++;
+	}
 
-    return (s);
+	return (s);
 }
 
 /**
@@ -31,13 +31,13 @@ char *cap_string(char *s)
  */
 int check_seperators(char c)
 {
-    int i = 0;
-    char seperators[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?',
-                           '"', '(', ')', '{', '}'};
-    for (; i < 13; i++)
-    {
-        if (c == seperators[i])
-            return (1);
-    }
-    return (0);
+	int i = 0;
+	char seperators[13] = { ' ', '\t', '\n', ',', ';', '.', '!', '?',
+		'"', '(', ')', '{', '}' };
+	for (; i < 13; i++)
+	{
+		if (c == seperators[i])
+			return (1);
+	}
+	return (0);
 }

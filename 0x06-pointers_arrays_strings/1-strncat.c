a2d37738606e34b69ea6d744c02ffc6a7fd23b82
Davids Chijioke Prince <77537310+cj-prince@users.noreply.github.com>
2022-03-24 15:47:51 +0100

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,13 +10,13 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-    int len, i;
+	int len,  i;
 
-    len = strlen(dest);
-    for (i = 0; i < n && src[i] != '\0'; i++)
-    {
-        dest[len + i] = src[i];
-    }
-    dest[len + i] = '\0';
-    return dest;
+	len = strlen(dest);
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[len + i] = src[i];
+	}
+	dest[len + i] = '\0';
+	return (dest);
 }

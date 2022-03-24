aa16c700f171c3036f27567139d348bfbe3ebf13
Davids Chijioke Prince <77537310+cj-prince@users.noreply.github.com>
2022-03-24 16:09:39 +0100

    betty error

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,13 +8,13 @@
  */
 char *string_toupper(char *s)
 {
-    char *start = s;
+	char *start = s;
 
-    while (*s)
-    {
-        if (*s >= 'a' && *s <= 'z')
-            *s -= 32;
-        s++;
-    }
-    return (start);
+	while (*s)
+	{
+		if (*s >= 'a' && *s <= 'z')
+			*s -= 32;
+		s++;
+	}
+	return (start);
 }

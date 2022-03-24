ee28f4d954f5f3d86946bcc3cb79fbc755089eae
Davids Chijioke Prince <77537310+cj-prince@users.noreply.github.com>
2022-03-24 16:24:09 +0100

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,11 +1,11 @@
 #include "main.h"
-#include "string.h"
+
 /**
- * _strncpy - to copy string from one to another 
- * @dest: the string to be pasted on;
- * @src: the string to be copied
- * @n: the number char we need to copy
- * Return: a string that points to a char copied
+ * _strncpy - copies a string
+ * @dest: copy destination
+ * @src: copy source
+ * @n: bytes to be copied
+ * Return: copied string
  */
 char *_strncpy(char *dest, char *src, int n)
 {

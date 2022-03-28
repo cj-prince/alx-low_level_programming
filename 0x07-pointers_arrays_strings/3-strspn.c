e4143782f3594d1f020c46ec9b794581e7ba8093
Davids Chijioke Prince <77537310+cj-prince@users.noreply.github.com>
2022-03-28 14:03:01 +0100

    betty error

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,24 +8,24 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-    unsigned int res = 0;
-    int i, foundChar;
+	unsigned int res = 0;
+	int i, foundChar;
 
-    while (*s)
-    {
-        for (i = 0; accept[i]; i++)
-        {
-            foundChar = 0;
-            if (*s == accept[i])
-            {
-                res++;
-                foundChar = 1;
-                break;
-            }
-        }
-        if (!foundChar)
-            break;
-        s++;
-    }
-    return (res);
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			foundChar = 0;
+			if (*s == accept[i])
+			{
+				res++;
+				foundChar = 1;
+				break;
+			}
+		}
+		if (!foundChar)
+			break;
+		s++;
+	}
+	return (res);
 }

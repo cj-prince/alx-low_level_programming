3c3299504a426a3ae286400aa58ff7c252f7c5cd
Davids Chijioke Prince <77537310+cj-prince@users.noreply.github.com>
2022-03-28 14:11:11 +0100

    Update 8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,13 +8,13 @@
  */
 void print_diagsums(int *a, int size)
 {
-    int i, j, sum = 0;
+	int i, j, sum = 0;
 
-    for (i = 0; i < size; i++)
-        sum += *(a + i * size + i);
-    printf("%i, ", sum);
-    sum = 0;
-    for (j = 0; j < size; j++)
-        sum += *(a + size * (j + 1) - (j + 1));
-    printf("%i\n", sum);
+	for (i = 0; i < size; i++)
+		sum += *(a + i * size + i);
+	printf("%i, ", sum);
+	sum = 0;
+	for (j = 0; j < size; j++)
+		sum += *(a + size * (j + 1) - (j + 1));
+	printf("%i\n", sum);
 }

d9133a4b5b1d0bfedda3aa17d1523baa2d323960
Davids Chijioke Prince <77537310+cj-prince@users.noreply.github.com>
2022-03-30 18:11:10 +0100

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -9,11 +9,11 @@ int helper_palindrome(char *s, int len);
  */
 int _strlen_recursion(char *s)
 {
-    if (!*s)
-    {
-        return (0);
-    }
-    return (_strlen_recursion(s + 1) + 1);
+	if (!*s)
+	{
+		return (0);
+	}
+	return (_strlen_recursion(s + 1) + 1);
 }
 
 /**
@@ -23,12 +23,12 @@ int _strlen_recursion(char *s)
  */
 int is_palindrome(char *s)
 {
-    int len;
+	int len;
 
-    len = _strlen_recursion(s);
-    if (len <= 1)
-        return (1);
-    return (helper_palindrome(s, len));
+	len = _strlen_recursion(s);
+	if (len <= 1)
+		return (1);
+	return (helper_palindrome(s, len));
 }
 
 /**
@@ -39,12 +39,13 @@ int is_palindrome(char *s)
  */
 int helper_palindrome(char *s, int len)
 {
-    if (len <= 1)
-        return (1);
-    else if (*s == *(s + len - 1))
-    {
-        return (helper_palindrome(s + 1, len - 2));
-    }
-    else
-        return (0);
+	if (len <= 1)
+		return (1);
+	else if (*s == *(s + len - 1))
+	{
+		return (helper_palindrome(s + 1, len - 2));
+	}
+	else
+		return (0);
+
 }

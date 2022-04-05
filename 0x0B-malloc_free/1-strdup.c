f9fa34eae9b11b45d04e3df478b90f762840f1fe
Davids Chijioke Prince <77537310+cj-prince@users.noreply.github.com>
2022-04-05 18:54:46 +0100

    betty error

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,34 +9,34 @@
  */
 char *_strdup(char *str)
 {
-    char *new_str, *start;
-    int i = 0, len = 0;
+	char *new_str, *start;
+	int i = 0, len = 0;
 
-    if (str == NULL)
-        return (NULL);
+	if (str == NULL)
+		return (NULL);
 
-    start = str;
+	start = str;
 
-    while (*str)
-    {
-        len++;
-        str++;
-    }
+	while (*str)
+	{
+		len++;
+		str++;
+	}
 
-    str = start;
-    new_str = malloc(sizeof(char) * (len + 1));
-    start = new_str;
+	str = start;
+	new_str = malloc(sizeof(char) * (len + 1));
+	start = new_str;
 
-    if (new_str != NULL)
-    {
-        for (; i < len; i++)
-        {
-            new_str[i] = *str;
-            str++;
-        }
-        new_str[i] = '\0';
-        return (start);
-    }
-    else
-        return (NULL);
+	if (new_str != NULL)
+	{
+		for (; i < len; i++)
+		{
+			new_str[i] = *str;
+			str++;
+		}
+		new_str[i] = '\0';
+		return (start);
+	}
+	else
+		return (NULL);
 }

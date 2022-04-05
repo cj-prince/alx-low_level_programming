44359a2df784392841015cbd3aa4766f3bff42e2
Davids Chijioke Prince <77537310+cj-prince@users.noreply.github.com>
2022-04-05 18:53:45 +0100

    betty error

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,21 +11,21 @@
 
 char *create_array(unsigned int size, char c)
 {
-    char *s;
-    unsigned int i = 0;
+	char *s;
+	unsigned int i = 0;
 
-    s = malloc(size * sizeof(char));
+	s = malloc(size * sizeof(char));
 
-    if (s == NULL)
-        return (NULL);
+	if (s == NULL)
+		return (NULL);
 
-    if (size == 0)
-        return (NULL);
+	if (size == 0)
+		return (NULL);
 
-    while (i < size)
-    {
-        s[i] = c;
-        i++;
-    }
-    return (s);
+	while (i < size)
+	{
+		s[i] = c;
+		i++;
+	}
+	return (s);
 }

1b9451b877177b70eb41f6bcce1b82a776020d63
Davids Chijioke Prince <77537310+cj-prince@users.noreply.github.com>
2022-04-05 18:56:22 +0100

    betty error fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,32 +11,32 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-    int len1, len2, lenT, i = 0, j;
-    char *p;
+	int len1, len2, lenT, i = 0, j;
+	char *p;
 
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
-    len1 = strlen(s1);
-    len2 = strlen(s2);
-    lenT = len1 + len2;
-    p = (char *)malloc(sizeof(char) * lenT + 1);
-    if (p == NULL)
-        return (NULL);
-    if (s1 != NULL)
-    {
-        for (; i < len1; i++)
-        {
-            p[i] = s1[i];
-        }
-    }
-    if (s2 != NULL)
-    {
-        for (j = 0; j < len2; j++)
-        {
-            p[i + j] = s2[j];
-        }
-    }
-    return (p);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+	lenT = len1 + len2;
+	p = (char *)malloc(sizeof(char) * lenT + 1);
+	if (p == NULL)
+		return (NULL);
+	if (s1 != NULL)
+	{
+		for (; i < len1; i++)
+		{
+			p[i] = s1[i];
+		}
+	}
+	if (s2 != NULL)
+	{
+		for (j = 0; j < len2; j++)
+		{
+			p[i + j] = s2[j];
+		}
+	}
+	return (p);
 }

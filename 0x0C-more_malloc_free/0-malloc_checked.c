7439a74280a42f54836b855d254512ab1df6a684
Davids Chijioke Prince <77537310+cj-prince@users.noreply.github.com>
2022-04-07 21:33:36 +0100

    Update 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -7,10 +7,10 @@
  */
 void *malloc_checked(unsigned int b)
 {
-    void *s;
+	void *s;
 
-    s = malloc(b);
-    if (s == NULL)
-        exit(98);
-    return (s);
+	s = malloc(b);
+	if (s == NULL)
+		exit(98);
+	return (s);
 }

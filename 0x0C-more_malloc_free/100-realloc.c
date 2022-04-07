c81c176e527661fc24eaafe14c5ace89bcdf930c
Davids Chijioke Prince <77537310+cj-prince@users.noreply.github.com>
2022-04-07 21:43:09 +0100

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -9,36 +9,36 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-    unsigned char *p;
-    unsigned int i;
+	unsigned char *p;
+	unsigned int i;
 
-    if (new_size == old_size)
-        return (ptr);
-    if (new_size == 0 && ptr != NULL)
-    {
-        free(ptr);
-        return (NULL);
-    }
-    p = malloc(new_size);
-    if (p == NULL)
-        return (NULL);
-    if (ptr == NULL)
-    {
-        return (p);
-    }
-    if (new_size > old_size)
-    {
-        for (i = 0; i < old_size; i++)
-            p[i] = ((char *)ptr)[i];
-        free(ptr);
-        return (p);
-    }
-    if (new_size < old_size)
-    {
-        for (i = 0; i < new_size; i++)
-            p[i] = ((char *)ptr)[i];
-        free(ptr);
-        return (p);
-    }
-    return (NULL);
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	p = malloc(new_size);
+	if (p == NULL)
+		return (NULL);
+	if (ptr == NULL)
+	{
+		return (p);
+	}
+	if (new_size > old_size)
+	{
+		for (i = 0; i < old_size; i++)
+			p[i] = ((char *)ptr)[i];
+		free(ptr);
+		return (p);
+	}
+	if (new_size < old_size)
+	{
+		for (i = 0; i < new_size; i++)
+			p[i] = ((char *)ptr)[i];
+		free(ptr);
+		return (p);
+	}
+	return (NULL);
 }

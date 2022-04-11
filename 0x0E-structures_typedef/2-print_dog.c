538e09d15708ee9554fa415afd9ed1b7ba8a5c53
Davids Chijioke Prince <77537310+cj-prince@users.noreply.github.com>
2022-04-11 16:48:08 +0100

    betty fix

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,12 +8,12 @@
  */
 void print_dog(struct dog *d)
 {
-    if (d == NULL)
-        return;
-    if (d->name == NULL)
-        d->name = "(nil)";
-    if (d->owner == NULL)
-        d->owner = "(nil)";
+	if (d == NULL)
+		return;
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
 
-    printf("Name: %s\nAge: %0.6f\nOwner: %s\n", d->name, d->age, d->owner);
+	printf("Name: %s\nAge: %0.6f\nOwner: %s\n", d->name, d->age, d->owner);
 }

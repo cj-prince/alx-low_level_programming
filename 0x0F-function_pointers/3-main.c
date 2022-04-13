a36111417c23380abaac1ece421c2f85a5d58f35
Davids Chijioke Prince <77537310+cj-prince@users.noreply.github.com>
2022-04-13 15:59:53 +0100

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,21 +11,21 @@
  */
 int main(int argc, char *argv[])
 {
-    int (*func)(int, int);
+	int (*func)(int, int);
 
-    if (argc != 4)
-    {
-        printf("Error\n");
-        exit(98);
-    }
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-    func = get_op_func(argv[2]);
-    if (func == NULL)
-    {
-        printf("Error\n");
-        exit(99);
-    }
+	func = get_op_func(argv[2]);
+	if (func == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-    printf("%d\n", func(atoi(argv[1]), atoi(argv[3])));
-    return (0);
+	printf("%d\n", func(atoi(argv[1]), atoi(argv[3])));
+	return (0);
 }

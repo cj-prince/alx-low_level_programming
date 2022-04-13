c7dc6d8c70d7f990b53e0d77a2b571bbfc5d83c8
Davids Chijioke Prince <77537310+cj-prince@users.noreply.github.com>
2022-04-13 16:30:50 +0100

    Update function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,8 @@
+#ifndef FUNC_POINTERS
+#define FUNC_POINTERS
+
+#include <stdio.h>
 #include <stdlib.h>
-#ifndef FUNCTION_POINTER_H
-#define FUNCTION_POINTER_H
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));

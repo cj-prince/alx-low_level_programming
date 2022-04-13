aa70c1d63b045849e9e4adb7498287c9aa90d5c9
Davids Chijioke Prince <77537310+cj-prince@users.noreply.github.com>
2022-04-13 15:57:49 +0100

    pointer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,20 +10,20 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-    int i = 0;
+	int i = 0;
 
-    if (cmp == NULL || array == NULL)
-        return (-1);
+	if (cmp == NULL || array == NULL)
+		return (-1);
 
-    if (size <= 0)
-        return (-1);
+	if (size <= 0)
+		return (-1);
 
-    for (; i < size; i++)
-        if (cmp(array[i]))
-            return (i);
+	for (; i < size; i++)
+		if (cmp(array[i]))
+			return (i);
 
-    if (i == size)
-        return (-1);
+	if (i == size)
+		return (-1);
 
-    return (-1);
+	return (-1);
 }

b9d8e18827f051142dbe8176bcd565829687c86a
Davids Chijioke Prince <77537310+cj-prince@users.noreply.github.com>
2022-04-13 15:56:51 +0100

    pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,11 +10,11 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-    unsigned int i = 0;
+	unsigned int i = 0;
 
-    if (action == NULL || array == NULL)
-        return;
+	if (action == NULL || array == NULL)
+		return;
 
-    for (; i < size; i++)
-        action(array[i]);
+	for (; i < size; i++)
+		action(array[i]);
 }

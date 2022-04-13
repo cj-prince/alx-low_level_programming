7edeca4d4b81e5ddaa49984bfb05982d5d75cab8
Davids Chijioke Prince <77537310+cj-prince@users.noreply.github.com>
2022-04-13 15:58:38 +0100

    function

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -10,20 +10,21 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
-    op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}};
-    int i = 0;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
 
-    while (i < 5)
-    {
-        if (*(ops[i]).op == *s && *(s + 1) == '\0')
-            return (ops[i].f);
-        i++;
-    }
-    return (NULL);
+	while (i < 5)
+	{
+		if (*(ops[i]).op == *s && *(s + 1) == '\0')
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
 }

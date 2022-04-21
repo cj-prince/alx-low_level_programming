42427f177c193d1a45bafe3383cb26ab9f9d7955
Davids Chijioke Prince <77537310+cj-prince@users.noreply.github.com>
2022-04-21 16:15:51 +0100

    betty error fix

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,19 +9,19 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-  list_t *temp;
-  int length = 0;
+	list_t *temp;
+	int length = 0;
 
-  temp = malloc(sizeof(list_t));
-  if (temp == NULL)
-    return (NULL);
+	temp = malloc(sizeof(list_t));
+	if (temp == NULL)
+		return (NULL);
 
-  while (str[length])
-    length++;
+	while (str[length])
+		length++;
 
-  temp->len = length;
-  temp->str = strdup(str);
-  temp->next = *head;
-  *head = temp;
-  return (temp);
+	temp->len = length;
+	temp->str = strdup(str);
+	temp->next = *head;
+	*head = temp;
+	return (temp);
 }

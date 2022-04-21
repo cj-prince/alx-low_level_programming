2c09df7d96b5e64316ab0910b25205c9d8a7c84c
Davids Chijioke Prince <77537310+cj-prince@users.noreply.github.com>
2022-04-21 16:18:03 +0100

    betty error fix

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -7,15 +7,15 @@
  */
 void free_list(list_t *head)
 {
-  list_t *temp;
+	list_t *temp;
 
-  temp = head;
-  while (head)
-  {
-    temp = head;
-    head = head->next;
-    free(temp->str);
-    free(temp);
-  }
-  free(head);
+	temp = head;
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+	free(head);
 }

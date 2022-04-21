b80c854c67613fec39a910647c4d7f45498ea228
Davids Chijioke Prince <77537310+cj-prince@users.noreply.github.com>
2022-04-21 16:17:22 +0100

    betty error fix

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -9,30 +9,30 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-  list_t *new_node, *temp;
-  unsigned int length = 0;
+	list_t *new_node, *temp;
+	unsigned int length = 0;
 
-  new_node = malloc(sizeof(list_t));
-  if (new_node == NULL)
-    return (NULL);
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
 
-  while (str[length])
-    length++;
+	while (str[length])
+		length++;
 
-  new_node->len = length;
-  new_node->str = strdup(str);
-  if (*head == NULL)
-  {
-    new_node->next = *head;
-    *head = new_node;
-  }
-  else
-  {
-    new_node->next = NULL;
-    temp = *head;
-    while (temp->next)
-      temp = temp->next;
-    temp->next = new_node;
-  }
-  return (new_node);
+	new_node->len = length;
+	new_node->str = strdup(str);
+	if (*head == NULL)
+	{
+		new_node->next = *head;
+		*head = new_node;
+	}
+	else
+	{
+		new_node->next = NULL;
+		temp = *head;
+		while (temp->next)
+			temp = temp->next;
+		temp->next = new_node;
+	}
+	return (new_node);
 }

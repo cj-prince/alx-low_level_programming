e976784afab6f8203803ab32456d8060f141006d
Davids Chijioke Prince <77537310+cj-prince@users.noreply.github.com>
2022-04-26 20:01:43 +0100

    betty error fix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,41 +9,41 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-  unsigned int i = 1;
-  listint_t *new_node, *temp;
+	unsigned int i = 1;
+	listint_t *new_node, *temp;
 
-  if (head == NULL)
-    return (NULL);
+	if (head == NULL)
+		return (NULL);
 
-  new_node = malloc(sizeof(listint_t));
-  if (new_node == NULL)
-    return (NULL);
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
 
-  if (*head == NULL)
-  {
-    *head = new_node;
-    new_node->next = NULL;
-    new_node->n = n;
-    return (new_node);
-  }
+	if (*head == NULL)
+	{
+		*head = new_node;
+		new_node->next = NULL;
+		new_node->n = n;
+		return (new_node);
+	}
 
-  if (idx == 0)
-  {
-    new_node->next = *head;
-    new_node->n = n;
-    *head = new_node;
-    return (new_node);
-  }
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		new_node->n = n;
+		*head = new_node;
+		return (new_node);
+	}
 
-  temp = *head;
-  while (i < idx)
-  {
-    temp = temp->next;
-    i++;
-  }
+	temp = *head;
+	while (i < idx)
+	{
+		temp = temp->next;
+		i++;
+	}
 
-  new_node->n = n;
-  new_node->next = temp->next;
-  temp->next = new_node;
-  return (new_node);
+	new_node->n = n;
+	new_node->next = temp->next;
+	temp->next = new_node;
+	return (new_node);
 }

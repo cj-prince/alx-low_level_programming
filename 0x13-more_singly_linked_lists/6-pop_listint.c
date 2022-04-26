fa35e420dbb5eef66088fdbd72561923217d3027
Davids Chijioke Prince <77537310+cj-prince@users.noreply.github.com>
2022-04-26 19:59:19 +0100

    error fix

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -8,15 +8,15 @@
  */
 int pop_listint(listint_t **head)
 {
-  listint_t *new_node;
-  int n;
+	listint_t *new_node;
+	int n;
 
-  if (*head == NULL)
-    return (0);
+	if (*head == NULL)
+		return (0);
 
-  n = (*head)->n;
-  new_node = (*head)->next;
-  free(*head);
-  *head = new_node;
-  return (n);
+	n = (*head)->n;
+	new_node = (*head)->next;
+	free(*head);
+	*head = new_node;
+	return (n);
 }

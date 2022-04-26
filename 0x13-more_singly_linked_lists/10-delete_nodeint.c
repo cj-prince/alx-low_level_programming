9741019566d92724331113040da06cb3e9110368
Davids Chijioke Prince <77537310+cj-prince@users.noreply.github.com>
2022-04-26 19:56:01 +0100

    betty fixing

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -11,31 +11,31 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-  listint_t *temp, *dnode;
-  unsigned int i = 1;
+	listint_t *temp, *dnode;
+	unsigned int i = 1;
 
-  if (*head == NULL)
-    return (-1);
+	if (*head == NULL)
+		return (-1);
 
-  temp = *head;
-  if (index == 0)
-  {
-    *head = (*head)->next;
-    free(temp);
-  }
-  else
-  {
-    while (i < index)
-    {
-      temp = temp->next;
-      if (temp == NULL)
-        return (-1);
-      i++;
-    }
-    dnode = temp;
-    dnode = dnode->next;
-    temp->next = dnode->next;
-    free(dnode);
-  }
-  return (1);
+	temp = *head;
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(temp);
+	}
+	else
+	{
+		while (i < index)
+		{
+			temp = temp->next;
+			if (temp == NULL)
+				return (-1);
+			i++;
+		}
+		dnode = temp;
+		dnode = dnode->next;
+		temp->next = dnode->next;
+		free(dnode);
+	}
+	return (1);
 }

d452cdc8634311735a421bad208ce5dd80e5d5e5
Davids Chijioke Prince <77537310+cj-prince@users.noreply.github.com>
2022-04-26 19:58:39 +0100

    error fix

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,17 +7,17 @@
  */
 void free_listint2(listint_t **head)
 {
-  listint_t *temp;
+	listint_t *temp;
 
-  if (head == NULL)
-    return;
+	if (head == NULL)
+		return;
 
-  temp = *head;
-  while (*head)
-  {
-    temp = *head;
-    (*head) = (*head)->next;
-    free(temp);
-  }
-  *head = NULL;
+	temp = *head;
+	while (*head)
+	{
+		temp = *head;
+		(*head) = (*head)->next;
+		free(temp);
+	}
+	*head = NULL;
 }

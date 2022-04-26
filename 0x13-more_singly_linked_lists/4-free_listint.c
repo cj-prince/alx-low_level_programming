19db743736ad696509f736044ca06b53da964040
Davids Chijioke Prince <77537310+cj-prince@users.noreply.github.com>
2022-04-26 19:58:01 +0100

    error fix

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -7,13 +7,13 @@
  */
 void free_listint(listint_t *head)
 {
-  listint_t *temp;
+	listint_t *temp;
 
-  while (head)
-  {
-    temp = head;
-    head = head->next;
-    free(temp);
-  }
-  free(head);
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+	free(head);
 }

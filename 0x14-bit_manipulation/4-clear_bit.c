b1d67f8d57165a6d7cf546174a5d1d2d72f72cfa
Davids Chijioke Prince <77537310+cj-prince@users.noreply.github.com>
2022-05-02 11:56:55 +0100

    Update 4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -8,11 +8,6 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-  unsigned long int max = 0x01;
-
-  max = ~(max << index);
-  if (max == 0x00)
-    return (-1);
-  *n &= max;
-  return (1);
+	*n = *n & ~(1 << index);
+	return ((index > 8 * sizeof(*n)) ? -1 : 1);
 }

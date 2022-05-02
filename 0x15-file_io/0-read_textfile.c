8b9199fbb6786b5cb9f4d2164b152a67107a72ec
Davids Chijioke Prince <77537310+cj-prince@users.noreply.github.com>
2022-05-02 18:25:32 +0100

    betty file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,40 +9,40 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-  int ID = 0;
-  ssize_t WR = 0;
-  ssize_t len = 0;
-  char *buff = NULL;
+	int ID = 0;
+	ssize_t WR = 0;
+	ssize_t len = 0;
+	char *buff = NULL;
 
-  if (filename != NULL)
-  {
-    buff = malloc(letters);
-    if (buff == NULL)
-    {
-      return (0);
-    }
+	if (filename != NULL)
+	{
+		buff = malloc(letters);
+		if (buff == NULL)
+		{
+			return (0);
+		}
 
-    ID = open(filename, O_RDONLY, 0600);
-    if (ID == -1)
-    {
-      free(buff);
-      return (0);
-    }
-    len = read(ID, buff, letters);
-    if (len == -1)
-    {
-      free(buff);
-      return (0);
-    }
-    WR = write(STDOUT_FILENO, buff, len);
-    if (WR == -1 || WR < len)
-    {
-      free(buff);
-      return (0);
-    }
-    close(ID);
-    free(buff);
-    return (WR);
-  }
-  return (0);
+		ID = open(filename, O_RDONLY, 0600);
+		if (ID == -1)
+		{
+			free(buff);
+			return (0);
+		}
+		len = read(ID, buff, letters);
+		if (len == -1)
+		{
+			free(buff);
+			return (0);
+		}
+		WR = write(STDOUT_FILENO, buff, len);
+		if (WR == -1 || WR < len)
+		{
+			free(buff);
+			return (0);
+		}
+		close(ID);
+		free(buff);
+		return (WR);
+	}
+	return (0);
 }

1979871f8f23ad5873c974dc026085ac5a5ed743
Davids Chijioke Prince <77537310+cj-prince@users.noreply.github.com>
2022-05-02 11:55:51 +0100

    Update 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -8,11 +8,8 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-  unsigned long int max = 0x01;
-
-  max <<= index;
-  if (max == 0)
-    return (-1);
-  *n |= max;
-  return (1);
+	if (index > (8 * sizeof(*n)) - 1)
+		return (-1);
+	*n = *n | (1 << index);
+	return (1);
 }

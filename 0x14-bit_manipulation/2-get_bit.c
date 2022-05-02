46f752abfc7acb0513fa903daf486387fe5c4d2b
Davids Chijioke Prince <77537310+cj-prince@users.noreply.github.com>
2022-05-02 11:55:10 +0100

    Update 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,14 +8,5 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-  unsigned long int max = 0x01;
-
-  max <<= index;
-  if (max == 0)
-    return (-1);
-
-  if ((n & max))
-    return (1);
-  else
-    return (0);
+	return ((index > (8 * sizeof(n) - 1)) ? -1 : (int)(n >> index) & 1);
 }

f36c30b16323a6c487211a7163684f807f0f3296
Davids Chijioke Prince <77537310+cj-prince@users.noreply.github.com>
2022-05-02 11:51:02 +0100

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,28 +1,34 @@
 #include "main.h"
-
+#include <string.h>
 /**
- * print_binary - A function that prints a binary number without % or /
- * @n: The number to be printed
- * Return: Nothing
+ * _stoi - converts chars to ints
+ * @c: char to convert
+ * Return: converted int
  */
-void print_binary(unsigned long int n)
+unsigned int _stoi(char c)
 {
-  unsigned int flag = 0, max = 32768; /* 1000 0000 0000 0000 */
+	return ((unsigned int) c - '0');
+}
+/**
+ * binary_to_uint - converts a string of 1's and 0's to a decimal number
+ * @b: string to convert
+ * Return: unsigned decimal number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	unsigned int result, tmp, expo;
 
-  if (n == 0)
-  {
-    _putchar('0');
-    return;
-  }
-  while (max)
-  {
-    if (flag == 1 && (n & max) == 0)
-      _putchar('0');
-    else if ((n & max) != 0)
-    {
-      _putchar('1');
-      flag = 1;
-    }
-    max >>= 1;
-  }
+	if (!b)
+		return (0);
+	result = tmp = 0;
+	expo = 1;
+	for (i = strlen(b) - 1; b[i]; i--, expo *= 2)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		tmp = _stoi(b[i]);
+		result += tmp * expo;
+	}
+	return (result);
 }

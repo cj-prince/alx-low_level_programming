b97db83884c1ffb35cd02c5d5805c566da4a9088
Davids Chijioke Prince <77537310+cj-prince@users.noreply.github.com>
2022-05-02 18:26:36 +0100

    fixed

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,25 +9,26 @@
 
 int create_file(const char *filename, char *text_content)
 {
-  int i = 0, file;
+	int i = 0, file;
 
-  if (filename == NULL)
-    return (-1);
+	if (filename == NULL)
+		return (-1);
 
-  if (text_content == NULL)
-    text_content = "";
+	if (text_content == NULL)
+		text_content = "";
 
-  while (text_content[i] != '\0')
-  {
-    i++;
-  }
 
-  file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	while (text_content[i] != '\0')
+	{
+		i++;
+	}
 
-  if (file == -1)
-    return (-1);
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-  write(file, text_content, i);
+	if (file == -1)
+		return (-1);
 
-  return (1);
+	write(file, text_content, i);
+
+	return (1);
 }

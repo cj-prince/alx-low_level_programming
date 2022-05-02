c96140f00e987e37de57210fd890dadc4da8b1ef
Davids Chijioke Prince <77537310+cj-prince@users.noreply.github.com>
2022-05-02 11:49:55 +0100

    Update 100-get_endianness.c

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -6,10 +6,10 @@
  */
 int get_endianness(void)
 {
-  int i;
-  char *test;
+	int i;
+	char *test;
 
-  i = 1;
-  test = (char *)&i;
-  return ((int)test[0]);
+	i = 1;
+	test = (char *)&i;
+	return ((int)test[0]);
 }

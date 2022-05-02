5e42e848271ac1e42d9412455f127d1ec4772e5a
Davids Chijioke Prince <77537310+cj-prince@users.noreply.github.com>
2022-05-02 11:51:50 +0100

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,30 +1,34 @@
 #include "main.h"
-
+#include <string.h>
 /**
- * binary_to_uint - A finction that converts binary number
- * to an unsigned integer
- * @b: A pointer to binary string
- * Return: unsigned integer converted number
+ * _stoi - converts chars to ints
+ * @c: char to convert
+ * Return: converted int
+ */
+unsigned int _stoi(char c)
+{
+	return ((unsigned int) c - '0');
+}
+/**
+ * binary_to_uint - converts a string of 1's and 0's to a decimal number
+ * @b: string to convert
+ * Return: unsigned decimal number
  */
 unsigned int binary_to_uint(const char *b)
 {
-  unsigned int base = 1, result = 0, len = 0;
+	int i;
+	unsigned int result, tmp, expo;
 
-  if (b == NULL)
-    return (0);
-
-  while (b[len])
-    len++;
-
-  while (len)
-  {
-    if (b[len - 1] != '0' && b[len - 1] != '1')
-      return (0);
-
-    if (b[len - 1] == '1')
-      result += base;
-    base *= 2;
-    len--;
-  }
-  return (result);
+	if (!b)
+		return (0);
+	result = tmp = 0;
+	expo = 1;
+	for (i = strlen(b) - 1; b[i]; i--, expo *= 2)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		tmp = _stoi(b[i]);
+		result += tmp * expo;
+	}
+	return (result);
 }
